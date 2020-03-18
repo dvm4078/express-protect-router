5469ba34b3f942b49e633ce6bb2659af24469ab7
Muoi Doan <dvm4078@gmail.com>
2020-03-18 12:13:20 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Express protect router with username, password
+
+## Features
+
+- Protect express router with username and password
+
+## Installation
+
+```shell
+yarn add express-protect-router
+```
+
+or
+
+```shell
+npm install express-protect-router --save
+```
+
+
+## Basic Usage
+
+```js
+import express from 'express';
+import Protect from 'express-protect-router';
+
+const app = express();
+
+const protecter = new Protect({ // pass into your configuration
+  username: 'username',
+  password: 'password',
+  isProtec: process.env.NODE_ENV === 'development' ? 1 : 0 , // you may want the route to be protected in different environments. For example protection on development and permission on producton
+});
+
+protecter.init(app);
+
+app.get('/', protecter.requireAuth, (req, res) => {
+  return res.status(200).json({ message: 'Welcome to protect router' });
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log(`
+    Port: ${3000}
+    Env: ${app.get('env')}
+  `);
+});
+
+export default app;
+```
+
+## Options
+
+| Prop                                                                   | Using with env var     |                     Default                     | Description                                                                                                                                                                                           |
+| :--------------------------------------------------------------------- | :------- | :----------------------------------------------: | :---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| `username` | USER_NAME |  | |
+| `password`  | PASSWORD |   | |
+| `iss`  | ISS | 'thisisiss' | Iss used to sign a token |
+| `expire`| EXPIRE | 60 * 60 * 24 (1 day) | Expire time of token |
+| `secret`| SECRET | 'SESSION_SECRET' | Secret key of token |
+| `isProtec`| PROTECT   |undefine| If isProtec is set to 0, authentication will be ignored |
+| `redirectTo`|  | '/' | The router will redirect after authentication |
+| `htmlPath` |  |  'static/login.html' | Path of the html file rather than the authentication form |
+| `protectPath` | | '/auth' | The router sends up the credentials|
---
+++ index.js
@@ -0,0 +1,70 @@
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
+class Protect {
+  constructor(configs = {}) {
+    this.username = configs.username || process.env.USER_NAME;
+    this.password = configs.password || process.env.PASSWORD;
+    this.iss = configs.iss || process.env.ISS || 'thisisiss';
+    this.expire = configs.expire || process.env.EXPIRE || 60 * 60 * 24; // 1 day
+    this.secret = configs.secret || process.env.SECRET || 'SESSION_SECRET';
+    this.isProtec = configs.isProtec || process.env.PROTECT;
+    this.redirectTo = configs.redirectTo || '/';
+    this.htmlPath = configs.htmlPath || 'static/login.html';
+    this.protectPath = configs.protectPath || '/auth';
+  }
+
+  init = (server) => {
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({ extended: true }));
+    server.use(cookieParser());
+
+    server.get(this.protectPath, this.requireUnAuth, (req, res) => {
+      res.sendFile(this.htmlPath, { root: __dirname });
+    });
+
+    server.post(this.protectPath, (req, res) => {
+      const { username, password } = req.body;
+      if (username === this.username && password === this.password) {
+        const token = jwt.sign({ username, password, iss: this.iss }, this.secret, {
+          expiresIn: this.expire,
+        });
+        res.cookie('X_ACCESS_TOKEN', token);
+        return res.end();
+      }
+      res.status(401);
+      return res.end();
+    });
+  }
+
+  requireAuth = (req, res, next) => {
+    if (this.isProtec) {
+      const token = req.cookies['X_ACCESS_TOKEN'];
+      jwt.verify(token, this.secret, async (err, decoded) => {
+        if (decoded && decoded.username === this.username && decoded.password === this.password && decoded.iss === this.iss) {
+          return next();
+        }
+        return res.redirect(this.protectPath);
+      });
+    } else {
+      next();
+    }
+  }
+
+  requireUnAuth = (req, res, next) => {
+    if (this.isProtec) {
+      const token = req.cookies['X_ACCESS_TOKEN'];
+      jwt.verify(token, this.secret, async (err, decoded) => {
+        if (decoded && decoded.username === this.username && decoded.password === this.password && decoded.iss === this.iss) {
+          return res.redirect(this.redirectTo);
+        }
+        return next();
+      });
+    } else {
+      next();
+    }
+  }
+}
+
+module.exports = Protect;
